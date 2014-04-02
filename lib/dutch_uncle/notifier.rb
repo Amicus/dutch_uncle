b8f9c70e2d415bbaad3e49e79afa62d95a6e6ab5
chrisvoxland <chrisvoxland@gmail.com>
2014-04-02 16:55:06 -0400

    change notifier

--- lib/dutch_uncle/notifier.rb
+++ lib/dutch_uncle/notifier.rb
@@ -1,6 +1,10 @@
 module DutchUncle
   class Notifier
 
+    def self.configure(config)
+      Honeybadger.configure { |config| config.api_key = config['api_key'] }
+    end
+
     def notify!(monitor_result)
       Honeybadger.notify({
         error_class: monitor_result.name,
